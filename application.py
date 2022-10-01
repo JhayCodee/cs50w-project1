4bb973cc64b619db0e26cff53e9a253ad0d031f4
Luis Vargas <kluis0178@gmail.com>
2022-10-01 01:15:44 -0600

    busqueda por isbn, titulo y autor terminada

--- application.py
+++ application.py
@@ -22,13 +22,34 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
 
-    book = db.execute("SELECT * FROM books").fetchone()
+    if 'user_id' in session:
 
-    return render_template("index.html", book=book)
+        if request.method == "POST":
 
+            search = request.form.get("search")
+
+            if not search:
+                return render_template("index.html", message="mensaje en blanco") 
+
+            else:
+                # consulta a la base de datos por isbn, titulo o autor ignorando el case sensitive (ilike)
+                books = db.execute("SELECT * FROM books WHERE isbn ILIKE :search OR title ILIKE :search OR author ILIKE :search", 
+                {"search": f"%{search}%"}).fetchall()
+                
+                return render_template("index.html", books=books, f=True)
+
+
+        else:
+            return render_template("index.html")
+
+    else:
+        return redirect(url_for('login'))
+
+
+''' LOGIN '''
 @app.route("/login", methods=["GET", "POST"])
 def login():
 
@@ -50,7 +71,7 @@ def login():
                 user = db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone()
 
                 if check_password_hash(user.password, password):
-                    #session["user_id"] = user.id
+                    session["user_id"] = user.id
                     print("usuario logueado")
                     return redirect(url_for("index"))
                 else:
@@ -60,27 +81,31 @@ def login():
         return render_template("login.html")
 
 
-
+''' REGISTER '''
 @app.route("/register", methods=["GET", "POST"])
 def register():
 
     if request.method == "POST":
 
+        # obtiene los datos del formulario
         username = request.form.get("username")
         password = request.form.get("password")
         cpassword = request.form.get("cf-password")
 
+        # verifica que los campos no esten vacios
         if not username or not password or not cpassword:
             print("falta informacion")
             return render_template("register.html")
         
         else:
 
+            # verifica que las contraseñas coincidan
             if password != cpassword:
                 print("no coinciden")
                 render_template("register.html")
             else:
 
+                # verifica que el usuario no exista
                 if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount == 0:
                 
                     '''Guardar en la base de datos'''
@@ -89,10 +114,12 @@ def register():
                     db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
                             {"username": username, "password": hash})
                     db.commit()
-                    
-                    print("user created")
-                    print(f"username: {username}, password: {hash}")
-                    redirect(url_for("login"))
+
+                    # guarda el id del usuario en la sesion
+                    user = db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone()
+                    session["user_id"] = user.id
+
+                    redirect(url_for("index"))
 
                 else:
                     print("usuario ya existe")
@@ -102,3 +129,11 @@ def register():
             
     else:
         return render_template("register.html")
+
+
+''' LOGOUT '''
+@app.route("/logout")
+def logout():
+    # limpia la sesion y redirige a la pagina de login
+    session.clear()
+    return redirect(url_for("index"))
