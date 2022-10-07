767d49c2840bac6f3b5e932f6cf40d0e1cdf8c41
Luis Vargas <kluis0178@gmail.com>
2022-10-06 23:39:23 -0600

    pagina de libro y api terminada

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
+import requests
 
-from flask import Flask, session, render_template, request, redirect, url_for, flash
+from flask import Flask, session, render_template, request, redirect, url_for, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -21,11 +22,15 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+#maneja errores 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route("/", methods=["GET", "POST"])
 def index():
 
-    if 'user_id' in session:
+    if 'user_id' in session: # verifica si el usuario esta logueado
 
         if request.method == "POST":
 
@@ -41,12 +46,135 @@ def index():
                 
                 return render_template("index.html", books=books, f=True)
 
-
         else:
+            #isbn='080213825X'
+            # response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbn).json()
+            # print(response["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"])
             return render_template("index.html")
 
     else:
         return redirect(url_for('login'))
+
+
+''' Books'''
+@app.route("/book/<string:book_isbn>", methods=["GET", "POST"])
+def book(book_isbn):
+    
+    # verifica si el usuario esta logueado
+    if 'user_id' in session: 
+
+        if request.method == "GET":
+            
+            # busca el libro por isbn en la base de datos
+            book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_isbn}).fetchone()
+
+            # si el libro no existe en la base de datos retorna 404
+            if book is None:
+                return "404"
+
+            else:
+
+                # consulta a la api de google books por el isbn del libro
+                response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:"+book_isbn).json()
+
+                # busca si existe un review del usuario logueado
+                user_review = db.execute("SELECT * FROM reviews WHERE book_id = :book_id AND user_id = :user_id",
+                {"book_id": book.id, "user_id": session['user_id']}).fetchone()
+                
+                # context donde almacenamos los datos del libro, de la base de datos y de la api
+                context = {
+                    "book": book,
+                    "img" : response["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"],
+                    "description": response["items"][0]["volumeInfo"]["description"],
+                    "isReview": True, #flag para las reviews de los usuarios
+                    "form": False   #flag para el formulario de reviews
+                }
+
+                if user_review is None:
+                    context["form"] = True
+
+                # buscar todas las reviews
+                reviews = db.execute("SELECT * FROM reviews WHERE book_id = :book_id", 
+                {"book_id": book.id}).fetchall()
+
+                if reviews is None:
+                    context["isReview"] = False
+                else:                                                                                                                 
+                    context["reviews"] = reviews
+                
+               
+                return render_template("book.html", **context)
+
+        elif request.method == "POST":
+
+            '''
+                buscar el libro en bd
+                ver si el user tiene una review en el libro
+                cargar reviews si hay
+            '''
+
+            review = request.form["review"]
+            rating = request.form["rating"]
+            try:
+                rating = int(rating)
+                print(rating)
+            except:
+                return "Rating no valido"
+
+
+            if not review or not rating:
+                return "Espacios en blanco"
+            else:
+
+                # busca el libro en la base de datos
+                book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_isbn}).fetchone()
+
+                #verificar si el usuario ya hizo un review
+                user_review = db.execute("SELECT * FROM reviews WHERE user_id = :user_id AND book_id = :book_id", 
+                {"user_id": session['user_id'], "book_id": book.id}).fetchone()
+
+                if user_review is None:
+                    #guardar el review
+                    db.execute("INSERT INTO reviews (review, book_id, user_id, rating) VALUES (:review, :book_id, :user_id, :rating)",
+                    {
+                        "review": review,
+                        "book_id": book.id,  
+                        "user_id": session['user_id'], 
+                        "rating": rating
+                    })
+
+                    db.commit()
+                    return redirect(url_for('book', book_isbn=book_isbn))
+                
+                else:
+                    return redirect(url_for('book', book_isbn=book_isbn))  
+        
+    else:
+        return redirect(url_for('login'))
+        
+@app.route("/api/<string:book_isbn>")
+def api(book_isbn):
+
+    if request.method == "GET":
+
+        book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_isbn}).fetchone()
+
+        if book is None:
+            return "404"
+        
+        review_count = db.execute("SELECT COUNT(*) FROM reviews WHERE book_id = :book_id", {"book_id": book.id}).fetchone()[0]
+        average_score = db.execute("SELECT AVG(rating) FROM reviews WHERE book_id = :book_id", {"book_id": book.id}).fetchone()[0]
+        
+        book_api = {
+            "title": book.title,
+            "author": book.author,
+            "year": book.year,
+            "isbn": book.isbn,
+            "review_count": review_count,
+            "average_score": round(average_score, 1)
+        }
+
+        return jsonify(book_api) 
 
 
 ''' LOGIN '''
@@ -64,11 +192,13 @@ def login():
         
         else:
 
-            if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount == 0:
+            if db.execute("SELECT * FROM users WHERE username = :username", 
+                        {"username": username}).rowcount == 0:
                 print("usuario no existe")
                 return render_template("login.html")
             else:
-                user = db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone()
+                user = db.execute("SELECT * FROM users WHERE username = :username", 
+                                {"username": username}).fetchone()
 
                 if check_password_hash(user.password, password):
                     session["user_id"] = user.id
@@ -137,3 +267,5 @@ def logout():
     # limpia la sesion y redirige a la pagina de login
     session.clear()
     return redirect(url_for("index"))
+
+
